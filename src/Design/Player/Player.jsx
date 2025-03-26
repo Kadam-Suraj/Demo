4bd9b6f351bc80411302372dee58fdfcfbe352fe
Kadam-Suraj <surajkadam381@gmail.com>
2025-03-26 19:27:28 +0530

    fix: update video import path and use dynamic source in Player component

--- src/Design/Player/Player.jsx
+++ src/Design/Player/Player.jsx
@@ -1,9 +1,9 @@
-// import video from '../../../assets/index';
+import video from '../../assets/index';
 
 const Player = () => {
     return (
         <div className='w-full'>
-            <video src={'src/assets/v1.mp4'} muted controls className="rounded-xl" />
+            <video src={video.mufasa} muted controls className="rounded-xl" />
         </div>
     )
 }
